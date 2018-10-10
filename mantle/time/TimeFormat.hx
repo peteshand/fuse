04e5d2ad54a2e35a446b2e97a1b817ca154f274a
P.J.Shand <pete@peteshand.net>
2018-10-10 12:56:14 +1100

    replace : with custom sep

--- mantle/time/TimeFormat.hx
+++ mantle/time/TimeFormat.hx
@@ -9,14 +9,14 @@ class TimeFormat
         return secToMinSec(TimeUtils.milToSeconds(milliseconds));
     }
 
-    public static function secToMinSec(seconds:Float):String
+    public static function secToMinSec(seconds:Float, sep:String=":"):String
     {
         var minutes:Int = Math.floor(seconds / 60);
         var sec:Int = Math.round(seconds - (minutes * 60));
-        return pad(minutes) + ":" + pad(sec);
+        return pad(minutes) + sep + pad(sec);
     }
 
-    public static function secToSecMilli(value:Float):String
+    public static function secToSecMilli(value:Float, sep:String=":"):String
     {
         var sec:Int = Math.floor(value / 1000);
         var milli:String = Std.string(value);
@@ -26,7 +26,7 @@ class TimeFormat
         if (l > 2) l = 2;
         milli = milli.substr(p, l);
         if (milli.length == 1) milli = "0" + milli;
-        return pad(sec) + ":" + milli;
+        return pad(sec) + sep + milli;
     }
 
     static function pad(value:Int):String
