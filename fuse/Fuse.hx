ac06eba33fe5d96dbe17b03d0991204f244c1aae
P.J.Shand <pete@peteshand.net>
2018-12-06 11:26:46 +1100

    -

--- fuse/Fuse.hx
+++ fuse/Fuse.hx
@@ -35,7 +35,8 @@ class Fuse extends EventDispatcher
 	public static var skipUnchangedFrames:Bool = true;
 	
 	public function new(rootClass:Class<Sprite>, fuseConfig:FuseConfig=null, stage3D:Stage3D=null, renderMode:Context3DRenderMode = AUTO, profile:Array<Context3DProfile> = null)
-	{	
+	{
+		trace("FUSE");
 		MessageManager.init();
 		if (fuseConfig == null) fuseConfig = { };
 		
